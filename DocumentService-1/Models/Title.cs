d9894d41a3f31006b30fcae61a48827b73d7ab49
KAM92W <KAM.92W@yandex.ru>
2024-04-05 14:45:12 +0300

    Add project files.

---
+++ DocumentService-1/Models/Title.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentService_1.Models
+{
+    public class Title
+    {
+        private string title { get; set; }
+        private string description { get; set; }
+
+        public void Show()
+        {
+            Console.WriteLine(title);
+            Console.WriteLine(description);
+        }
+    }
+}
